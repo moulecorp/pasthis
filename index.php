060a26d6acd410294dea7603a4695adfe67c7e99
Antoine Tenart <atenart@n0.pe>
2014-02-05 19:38:13 +0100

    Set the command line link to the local file

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ final class Pasthis {
             print $ct;
         print '<div id="footer">';
         print 'Powered by <a href="https://github.com/jvoisin/pasthis">pasthis</a> - ';
-        print '<a href="https://github.com/jvoisin/pasthis/blob/master/pasthis.pl">command line</a>';
+        print '<a href="./pasthis.pl">command line</a>';
         print '</div>';
         print '</body>';
         print '</html>';
